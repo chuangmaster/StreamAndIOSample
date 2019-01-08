26db9dc0e9990e2b4b198aaed0be8e4e3d273c6a
chuangmaster <masterchuang@urtody.com>
2019-01-08 16:51:45 +0800

    撰寫範例

--- StreamAndIOSample/Program.cs
+++ StreamAndIOSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace StreamAndIOSample
     {
         static void Main(string[] args)
         {
+            Stream stream = IOService.FileToStream(@"C:\\pn_514.png");
+            IOService.StreamToFile(@"D:\\pn_514.png", stream);
         }
     }
 }
